fee9cba081d06f2a507197eb81446355cd71ba67
Sergey Pakhomov <sergey.pakhomov.89@gmail.com>
2019-11-01 17:02:40 +0300

    Start popup testing

--- test_work.py
+++ test_work.py
@@ -2,24 +2,41 @@
 
 import os
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 class RunTest(unittest.TestCase):
 
   def setUp(self):
-    self.driver = webdriver.Chrome()
-    self.driver.implicitly_wait(30)
-    self.driver.maximize_window()
+    self.d = webdriver.Chrome()
+    self.d.maximize_window()
 
-    self.driver.get("http://dodopizza.ru")
+    self.d.get("http://dodopizza.ru")
 
 
-  def test_stub(self):
-    return True
+  def test_popup(self):
+    # popup_dialog = self.d.find_element_by_class_name('popup_dialog-inner')
+    # self.assertTrue(popup_dialog.is_displayed())
 
+    WebDriverWait(self.d, 5)\
+      .until(expected_conditions.visibility_of_element_located(\
+          (By.CLASS_NAME, "popup__dialog-inner")))
+
+    popup_logo = self.d.find_element_by_class_name('locality-selector-popup__logo')
+    self.assertTrue(popup_logo.is_displayed())
+
+    # todo: check dodo logo is what you need
+
+    popup_search = self.d.find_element_by_class_name('locality-selector-popup__search')
+    self.assertTrue(popup_search.is_displayed())
+
+    # todo: input by xpath or ?
 
   def tearDown(self):
-    self.driver.quit()
+    self.d.quit()
 
 if __name__ == '__main__':
   unittest.main(verbosity=2)
